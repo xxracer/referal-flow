ef586090f2bf0d22ec001a506546c135414415e4
Maijel Cancines <maijelcancines2@gmail.com>
2026-01-08 21:18:14 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,8 +4,9 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "onboard-panel-gx822.firebaseapp.com",
+  databaseURL: "https://onboard-panel-gx822-default-rtdb.firebaseio.com",
   projectId: "onboard-panel-gx822",
-  storageBucket: "onboard-panel-gx822.appspot.com",
+  storageBucket: "onboard-panel-gx822.firebasestorage.app",
   messagingSenderId: "872617759649",
-  appId: "1:872617759649:web:3822a76f24efc6e613f175",
+  appId: "1:872617759649:web:e2ade97d407b3498fc31e0"
 };
